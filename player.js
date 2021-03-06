ce9b4805cd8d85b7d301b0e2608d78585361b1b8
lavina <lavinakhushlani@gmail.com>
2021-03-06 11:50:45 +0530

    Fetch jsonp data from the given url and display data on console

--- player.js
+++ player.js
@@ -1,1 +1,7 @@
 // initialized player.js
+var script = document.createElement('script');
+script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+$.getJSON("https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=?",function(json){
+  console.log(json['data']['structure']['steps']);
+});
