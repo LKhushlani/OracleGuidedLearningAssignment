2947f1897ae174e4e77798623f2ecf185c96cf5e
lavina <lavinakhushlani@gmail.com>
2021-03-09 08:58:12 +0530

    bug fixes

--- player.js
+++ player.js
@@ -1,28 +1,60 @@
-// initialized player.js
+
+function loadScript() 
+{
 var script = document.createElement('script');
 script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
-script.src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js"
-document.getElementsByTagName('head')[0].appendChild(script);
-$.getJSON("https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=?",function(json){
-$('head').append('<link rel="stylesheet" href="https://guidedlearning.oracle.com/player/latest/static/css/stTip.css" type="text/css" />');
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(script, s);
+var script = document.createElement('script');
+script.src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js";
+s.parentNode.insertBefore(script, s);
 
-$.each(json['data']['structure']['steps'], function(index, element) {
-console.log("index", element['action']['contents']); 
+return new Promise((res, rej) => {
+    script.onload = function () {
+        $.getJSON("https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=?", function (json) {
+            $('head').append('<link rel="stylesheet" href="https://guidedlearning.oracle.com/player/latest/static/css/stTip.css" type="text/css" />');
 
-$(function() { 
-    $('.gb_g').tooltip({  
-       items: 'a.gb_g',  
-       content: '<div  class="sttip"><div class="tooltip in"><div class="tooltip-arrow"></div><div class="tooltip-arrow second-arrow"></div><div class="popover-inner"><p>Click <strong>Images</strong> to go to images section</p></div></div></div>',
-//        element['action']['contents']['#content'][1],  
-       show: "slideDown", 
-       open: function(event, ui)  
-       {  
-          ui.tooltip.hover(  
-          function () {  
-             $(this).fadeTo("slow", 0.5);  
-          });  
-       }  
-    }).addClass('showPrevBt');  
- });
-  });
-  });
+            $.each(json['data']['structure']['steps'], function (index, element) {
+                //  console.log("index", element['action']); 
+                var data = element['action'];
+                console.log(json['data']['tiplates'])
+
+                $(function () {
+                    //    console.log("data",data);
+                    var selector = data['selector'];
+                    var msg = ""
+                    if (data.hasOwnProperty('contents')) {
+                        msg = data['contents']['#content']
+                    }
+
+                    console.log(msg)
+                    // console.log("selec", data['selector']);
+                    var content_str = '<div  class="sttip"><div class="tooltip in"><div class="tooltip-arrow"></div><div class="tooltip-arrow second-arrow"></div><div class="popover-inner">' + json['data']['tiplates']['tip'] + '</div></div></div>';
+                    $(selector).tooltip({
+                        content: content_str,
+                        show: "slideDown",
+                        open: function (event, ui) {
+                            ui.tooltip.hover(
+                                function () {
+                                    $(this).fadeTo("slow", 0.5);
+                                });
+                        }
+                    }).addClass('showPrevBt');
+                });
+            });
+        });
+
+    }
+    script.onerror = function () {
+        console.log("Script Not Loaded")
+    }
+});
+}
+
+loadScript()
+.then(() => {
+    console.log('Script loaded!');
+})
+.catch(() => {
+    console.error('Script loading failed! Handle this error');
+});
